e279134bc2254fc7de40bf156e438573e5cf8d73
WENCHO22 <wencho22@gmail.com>
2021-01-19 04:48:26 +0100

    added ageify.js

---
+++ javascript/javascript1/week1/ageify.js
@@ -0,0 +1,9 @@
+//Excercise 1: ageify
+function ageify(yearOfBirth, yearFuture) {
+    let age = yearFuture - yearOfBirth;
+    return "You will be " + age + " years old in " + yearFuture;
+}
+
+ageify(1992, 2099);
+
+
