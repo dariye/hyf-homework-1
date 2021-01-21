24ef0a59268339bd0d957dc9ce6119a9a4d5415b
WENCHO22 <wencho22@gmail.com>
2021-01-21 01:31:31 +0100

    Improved functions on the js file

--- javascript/javascript1/week1/smart-ease.js
+++ javascript/javascript1/week1/smart-ease.js
@@ -10,7 +10,7 @@ ageify(1992, 2099);
 //Excercise 2: dog years
 
 function dogAge(dogYearOfBirth, dogYearFuture, shouldResultInDogYears) {
-    let dogYear = dogYearOfBirth - dogYearFuture;
+    let dogYear = dogYearFuture - dogYearOfBirth;
     if (shouldResultInDogYears === false) {        
         var humanOrDog = " human years old in "
     } else {
@@ -26,17 +26,20 @@ dogAge(2015, 2025, true);
 
 
 //Excercise 3: House price stimator
-function housePriceStimator(housePrice, volumeInMeters, gardenSizeInM2) {
+function housePriceStimator( houseWidth, houseDepth, houseHeight, gardenSizeInM2, housePrice) {
+    var volumeInMeters =  houseWidth * houseHeight * houseDepth;
     var fairPrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
     if (housePrice === fairPrice) {
-        return "The house price is fair"
+        return "The house price is fair."
     } else if (housePrice > fairPrice) {
-        return "The house price is over market value"
+        return "The house price is $" + (housePrice - fairPrice) +  " over market value.";
     } else {
-        return "The house price is below market value"
+        return "The house price is $" + (fairPrice - housePrice) +  " below market value."
     }
 }
 
+housePriceStimator(8, 10, 10, 100, 2500000); //for Peters house
+housePriceStimator(5, 11, 8, 70, 1000000); //For Julias house
 
 
 //Excercise 4: ez-namey
@@ -45,5 +48,6 @@ function nameYourCompany(){
     const secondWords =   ["Company", "Inc.", "A.S.", "Corporation", "Corp.", "Solutions", "Worldwide", "Agency", "Enterprises", "Agency"];
     const randomNumber = Math.floor(Math.random() * 10) + 0;
     const otherRandomNumber = Math.floor(Math.random() * 10) + 0;
-    return firstWords[randomNumber] + secondWords[otherRandomNumber]
+    const startUpName = firstWords[randomNumber] + secondWords[otherRandomNumber];
+    return 'The startup: "' + startUpName + '" cointains ' + startUpName.length + ' characters';
 }
